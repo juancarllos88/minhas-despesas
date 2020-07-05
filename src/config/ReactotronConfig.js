dc7df81d39e388ed3c3b44b782b89f3b5970a262
juancarllos88 <juancarllos.a@gmail.com>
2020-07-05 09:30:17 -0300

    Comitando fotos do projeto

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -2,7 +2,7 @@ import Reactotron from 'reactotron-react-native';
 
 
 if(__DEV__){
-  const tron = Reactotron.configure({ host: '192.168.1.6' }).useReactNative().connect();
+  const tron = Reactotron.configure({ host: '192.168.1.5' }).useReactNative().connect();
   tron.clear();
   console.tron = tron;
 }
